66f9176c43a3eb0be53135e0e364c7b2e1e1a21c
Tristan Viard <tviardprimo190@gmail.com>
2026-05-15 08:40:47 +0200

    fix gap between sidebar admin and content

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -7,7 +7,7 @@ export default async function AdminLayout({ children }: { children: React.ReactN
     if (!requireAdmin(pb)) redirect('/login');
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-5 gap-6 items-start">
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-4 items-start">
             <SidebarAdmin />
             <div className="md:col-span-4">{children}</div>
         </div>
